453782afd4b9712f4d264d02ea0aefdcf83c09c7
Srivathsa Rao <srivathsa.rao1@zensar.com>
2018-09-17 18:24:14 +0530

    making footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class Footer extends React.Component {
   constructor(){
     super();
   }
  render () {
+   const data = this.props.apiServiceReducer.data;
+   const head = data.footer && data.footer.NoSign && data.footer.NoSign.head ||"";
+   const info = data.footer && data.footer.NoSign && data.footer.NoSign.info ||"";
+   const ButtonText = data.footer && data.footer.NoSign && data.footer.NoSign.ButtonText ||"";  
+
+
    return (
-     <div>
-       Footer <br/>
-     </div>
+    <footer>
+    <div className="container-fluid common-footer-color">
+        <div className="container">
+            <div className="text-center">
+                <h1>{head}</h1>
+                <p>{info}</p>
+                <button type="button"  class="btn btnNosign btn-lg"><font-awesome-icon size="xs" icon="lock" /> {ButtonText}</button>
+            </div>
+        </div> 
+    </div>
+</footer>
    )
  }
 }
